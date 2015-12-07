16ced6458a3a0ec649e1bbf8e1415f7389ccfdef
Jorik <jorik@oostenbrink.org>
2015-12-07 15:45:05 +0100

    test classifiers af

--- testParzen.m
+++ testParzen.m
@@ -1,10 +1,28 @@
 function [besth,err] = testParzen(dataset,I,J)
-besth = -1;
+disp('testing parzen');
+epsilon = 0.01;
+
+besth = 2;
 err = inf;
 dif = inf;
-step = 0.1;
-steps = 30;
+step = 1;
+steps = 2;
 
-for h = 0:
+while (dif > epsilon)
+    olderr = err;
+    
+    for h = (besth-steps*step):step:(besth+steps*step)        
+        fprintf('h = %d\n',h);
+        e = testClassifier(dataset,I,J,parzenc([],h));
+        
+        if (err > e)
+            err = e;
+            besth = h;
+        end
+    end
+    
+    dif = olderr - err;
+    step = step/2.0;
+    steps = 2;
 end
 
