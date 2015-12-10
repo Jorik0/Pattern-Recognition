73b4fc37a92c477b2bd1d754954ac1f8772c03b8
Chantal Eckhardt <chantaleck94@gmail.com>
2015-12-10 10:56:50 +0100

    eventjes

--- moments.m
+++ moments.m
@@ -1,9 +1,9 @@
 function e = moments(im); %give im without 6's or 9's
-type = {'none','central','scaled','hu','zer'};
-%type = {'central','hu'};
+%type = {'none','central','scaled','hu','zer'};
+type = {'zer'};
 c = [];
 for i = 1:size(type,2)
-c = [c im_moments(im,type{i})];
+c = [c im_moments(im,type{i},12)];
 end
 
 e = testClassifiers(c,200);
