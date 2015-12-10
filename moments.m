94c0e319d0afe42cc20e58f76762f81c0b475cdd
Chantal Eckhardt <chantaleck94@gmail.com>
2015-12-10 12:16:02 +0100

    cartesian product

--- moments.m
+++ moments.m
@@ -1,9 +1,14 @@
-function e = moments(im); %give im without 6's or 9's
+function e = moments(im); 
 %type = {'none','central','scaled','hu','zer'};
-type = {'zer'};
+type = {'none'};
 c = [];
+
+sets = {[0 1 2 3 4],[0 1 2 3 4]};
+[x y] = ndgrid(sets{:});
+cartProd = [x(:) y(:)];
+
 for i = 1:size(type,2)
-c = [c im_moments(im,type{i},12)];
+c = [c im_moments(im,type{i},cartProd)];
 end
 
 e = testClassifiers(c,200);
