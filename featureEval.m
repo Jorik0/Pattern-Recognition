f1a6bab165bfb212d56180b51c2ba34748dfb72c
Jorik Oostenbrink <jorik@oostenbrink.org>
2016-01-13 09:54:51 +0100

    Bugfix

--- featureEval.m
+++ featureEval.m
@@ -8,19 +8,17 @@ end
 error = 0;
 aError = 0;
 for i = 1:ntimes
-    [train test] = gendat(ds,ones(1,10)*size);
-    %w = featseli(train,crit); %Orders the features 
-    %w = featseli(train,crit,0,ntimes); %Use cross-validation for classifiers as crit
+    [train, test] = gendat(ds,ones(1,10)*size);
     w = train*modifier;
     E = clevalf(train*w,classifiers,featsizes,ones(1,10)*size,1,test*w);
     
     error = error + E.error;
-    aError = aError + aError;
+    aError = aError + E.apperror;
     fprintf('Experiment %d/%d complete\n',i,ntimes);
 end
 
 E.error = error/ntimes;
-E.aError = aError/ntimes;
+E.apperror = aError/ntimes;
 E.ylabel = sprintf('Averaged error (%d experiments)',ntimes);
 E.plot = 'plot';
 
