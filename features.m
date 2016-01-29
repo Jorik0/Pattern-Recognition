19a02e0e449662bb6cda3c45df3acab4d31297d8
Chantal Eckhardt <chantaleck94@gmail.com>
2016-01-29 16:19:45 +0100

    features function, des function

--- features.m
+++ features.m
@@ -1,8 +1,20 @@
-function e = features(im);
+function c = features(im);
 prwarning off;
 prwaitbar off;
-c = im_features(im,im,'Area');
-%'Area','Perimeter'
+c1 = im_features(im,im,'all');
 
-%c = im_mean(im);
-e = testClassifiers(c,10);
+sets = {[0 1 2 3 4],[0 1 2 3 4]};
+[x y] = ndgrid(sets{:});
+cartProd = [x(:) y(:)];
+
+c2 = im_moments(im,'none',cartProd);
+c3 = im_moments(im,'central',cartProd);
+
+sets = {[0 1 2 3 4 5 6 7 8 9],[0 1 2 3 4 5 6 7 8 9]};
+[x y] = ndgrid(sets{:});
+cartProd = [x(:) y(:)];
+
+c4 = im_moments(im,'scaled',cartProd);
+c5 = im_profile(im);
+c = [c1 c2 c3 c4 c5];
+
